8625302372b82289a4195e6e1c4d0f846be13117
Thomas Weißschuh <thomas.weissschuh@de.amadeus.com>
2015-01-15 12:59:53 +0000

    remove unsed imports

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build.Dependencies import cythonize
-import imp
-import os
 
 setup(
     name = 'ptypes',
